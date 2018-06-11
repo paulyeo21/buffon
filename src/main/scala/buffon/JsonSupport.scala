30e9edeba962d11e9f78e320855f4316eff979b3
Paul Yeo <paulyeo21@gmail.com>
2018-06-11 10:40:45 -0700

    Add sizes field (#10)

--- src/main/scala/buffon/JsonSupport.scala
+++ src/main/scala/buffon/JsonSupport.scala
@@ -13,7 +13,8 @@ case class ShoeListing(
   sku: Long,
   description: String,
   condition: String,
-  gender: String
+  gender: String,
+  sizes: Seq[Float]
 )
 case class User(email: String, password: String)
 case class RefreshToken(selector: String, hash: String, expiration: Long, sessionData: String)
@@ -21,7 +22,7 @@ case class ShoeListings(shoeListings: Array[ShoeListing])
 case class SearchPayload(q: String, from: Int, size: Int, filters: Map[String, Seq[String]])
 
 trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
-  implicit val shoeListingFormat = jsonFormat7(ShoeListing)
+  implicit val shoeListingFormat = jsonFormat8(ShoeListing)
   implicit val userFormat = jsonFormat2(User)
   implicit val refreshTokenFormat = jsonFormat4(RefreshToken)
   implicit val shoeListingsFormat = jsonFormat1(ShoeListings)
